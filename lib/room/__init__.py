ffda3d588b64b3925d4c22b359055589bda9b2c4
WangChengYeh <cywang.eda@gmail.com>
2026-05-09 11:26:23 +0800

    Export room dispatcher and iMessage helpers

--- lib/room/__init__.py
+++ lib/room/__init__.py
@@ -1,14 +1,32 @@
 from __future__ import annotations
 
+from .dispatcher import RoomDispatcher, RoomDispatchError, RoomDispatchRequest, RoomDispatchResult
+from .imessage_correlation import IMessageCorrelationBinding, IMessageCorrelationStore
+from .imessage_delivery import RoomIMessageDelivery, RoomIMessageDeliveryPolicy, RoomIMessageDeliveryResult
+from .imessage_dispatch import IMessageDispatchBridge, IMessageDispatchResult
 from .models import RoomEvent, RoomEventType, RoomSource, RoomValidationError
 from .parser import ParsedRoomCommand, RoomCommandError, parse_room_command
+from .store import RoomEventStore, RoomStoreError
 
 __all__ = [
+    'IMessageCorrelationBinding',
+    'IMessageCorrelationStore',
+    'IMessageDispatchBridge',
+    'IMessageDispatchResult',
     'ParsedRoomCommand',
     'RoomCommandError',
+    'RoomDispatcher',
+    'RoomDispatchError',
+    'RoomDispatchRequest',
+    'RoomDispatchResult',
     'RoomEvent',
+    'RoomEventStore',
     'RoomEventType',
+    'RoomIMessageDelivery',
+    'RoomIMessageDeliveryPolicy',
+    'RoomIMessageDeliveryResult',
     'RoomSource',
+    'RoomStoreError',
     'RoomValidationError',
     'parse_room_command',
 ]
